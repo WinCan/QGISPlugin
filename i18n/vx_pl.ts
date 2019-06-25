5204ad271e7c28d391a485ac25bc28c3a5e8a5f2
PatrykPaziewski <52149118+PatrykPaziewski@users.noreply.github.com>
2019-06-25 12:44:22 +0200

    Polski lang

---
+++ i18n/vx_pl.ts
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL" sourcelanguage="en">
+<context>
+    <name>VX</name>
+    <message>
+        <location filename="../VX_integration.py" line="149"/>
+        <source>&amp;WinCan VX integration</source>
+        <translatorcomment>Integracja z WinCan VX</translatorcomment>
+        <translation>Integracja z WinCan VX</translation>
+    </message>
+    <message>
+        <location filename="../VX_integration.py" line="137"/>
+        <source>Open dialog</source>
+        <translation>Otwórz okienko</translation>
+    </message>
+</context>
+<context>
+    <name>VXDialogBase</name>
+    <message>
+        <location filename="../Second_window.ui" line="29"/>
+        <source>Fields Mapping</source>
+        <translation>Mapowanie pól</translation>
+    </message>
+    <message>
+        <location filename="../Second_window.ui" line="76"/>
+        <source>&lt; Add</source>
+        <translation>&lt;- Dodaj</translation>
+    </message>
+    <message>
+        <location filename="../Second_window.ui" line="143"/>
+        <source>WinCan Fields</source>
+        <translation>Pola WinCan</translation>
+    </message>
+    <message>
+        <location filename="../Second_window.ui" line="156"/>
+        <source>Available Fields</source>
+        <translation>Dostępne Pola</translation>
+    </message>
+    <message>
+        <location filename="../Second_window.ui" line="169"/>
+        <source>Layer Fields</source>
+        <translation>Pola warstwy</translation>
+    </message>
+    <message>
+        <location filename="../Second_window.ui" line="208"/>
+        <source>WinCan</source>
+        <translation>WinCan</translation>
+    </message>
+    <message>
+        <location filename="../Second_window.ui" line="213"/>
+        <source>Map</source>
+        <translation>Mapa</translation>
+    </message>
+    <message>
+        <location filename="../Second_window.ui" line="227"/>
+        <source>Field Mapping</source>
+        <translation>Mapowanie pól</translation>
+    </message>
+    <message>
+        <location filename="../Second_window.ui" line="253"/>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <location filename="../Second_window.ui" line="267"/>
+        <source>Remove &gt;</source>
+        <translation>Usuń -&gt;</translation>
+    </message>
+    <message>
+        <location filename="../VX_integration_dialog_base.ui" line="32"/>
+        <source>WinCan VX integration</source>
+        <translation>Integracja z WinCan VX</translation>
+    </message>
+    <message>
+        <location filename="../VX_integration_dialog_base.ui" line="67"/>
+        <source>Connect</source>
+        <translation>Połącz</translation>
+    </message>
+    <message>
+        <location filename="../VX_integration_dialog_base.ui" line="83"/>
+        <source>To WinCan VX --&gt;</source>
+        <translation>Do WinCan VX -&gt;</translation>
+    </message>
+    <message>
+        <location filename="../VX_integration_dialog_base.ui" line="124"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Project name:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Nazwa projektu:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../VX_integration_dialog_base.ui" line="169"/>
+        <source>Reinitialize</source>
+        <translation>Reinicjalizuj</translation>
+    </message>
+</context>
+</TS>
